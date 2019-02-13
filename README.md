a72ed0056324ca3c1e73f56ea593700ce3c20c33
José Luis González Beltrán <joseluis.gonzalezbeltran@externals.adidas-group.com>
2019-02-13 16:19:02 +0100

    (feature) enable security in Swagger ui and document README

--- README.md
+++ README.md
@@ -1,9 +1,51 @@
 # coding-challenge-city-data
 
-## how to run mongodb
+This is a Spring Boot REST application.
+- Manages security with Spring Security
+- Configuration is stored independent in a git repository using Spring Cloud Config
+- Active endpoints can be tested with Swagger
+- Uses mongoDB as database to store cities information
+
+## swagger
+
+Swagger UI: http://localhost:8090/swagger-ui.html
+
+## spring security
+
+*Users:*
+
+user/userPass
+admin/adminPass
+
+## spring cloud config on docker (how to)
+
+*Against remote git repository:*
+```bash
+$ docker run -d -p 8888:8888 \
+    --net codingchallenge \
+    --name spring-cloud-config \
+    -e SPRING_CLOUD_CONFIG_SERVER_GIT_URI=<uri> \
+    -e SPRING_CLOUD_CONFIG_SERVER_GIT_USERNAME=<user> \
+    -e SPRING_CLOUD_CONFIG_SERVER_GIT_PASSWORD=<password> \
+    -e SPRING_CLOUD_CONFIG_SERVER_GIT_CLONE_ON_START=true \
+    hyness/spring-cloud-config-server:2.1.0.RELEASE
+```
+*Against local git repository:*
+```bash
+$ docker run -d -p 8888:8888 \
+    --net codingchallenge \
+    --name spring-cloud-config \
+    -v /path/to/config/files/dir:/config \
+    -e SPRING_PROFILES_ACTIVE=native \
+    hyness/spring-cloud-config-server:2.1.0.RELEASE
+```
+
+## mongodb on docker (how to)
 
 ```bash
-$ docker run -d --net codingchallenge --name mongodb1 \
+$ docker run -d -p 27017:27017 \
+    --net codingchallenge
+    --name mongodb1 \
     -v /home/jgb/projects/coding-challenge/coding-challenge-mongodb-datadir:/data/db \
     -p 27017:27017 \
     mongo:4.0
--- src/main/java/org/jgb/citydata/config/SwaggerConfig.java
+++ src/main/java/org/jgb/citydata/config/SwaggerConfig.java
@@ -1,16 +1,22 @@
 package org.jgb.citydata.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 /**
  * @author jgb
@@ -18,26 +24,35 @@ import java.util.Collections;
  */
 @Configuration
 @EnableSwagger2
+@EnableWebSecurity
 public class SwaggerConfig {
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("org.jgb.citydata.controller"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.ant("/api/v1/**"))
                 .build()
                 .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
-        return new ApiInfo("My REST API",
-                "Some custom description of API.",
-                "API v1",
-                "Terms of service",
-                new Contact("Jose Luis Gonzalez Beltran",
+        return new ApiInfoBuilder()
+                .title("Cities Data REST API")
+                .description("REST API for retrieving city information.")
+                .version("API v1")
+                .contact(new Contact("Jose Luis Gonzalez Beltran",
                         "www.jgb.info",
-                        "joseluis.gonzalez@darecode.com"),
-                "License of API",
-                "API license URL", Collections.emptyList());
+                        "jlgonzalezbeltran@outlook.com"))
+                .build();
+    }
+
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .docExpansion(DocExpansion.LIST)
+                .defaultModelRendering(ModelRendering.MODEL)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS).build();
     }
 }
