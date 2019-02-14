d31989b744d0ed8df4dad31242a577e262a7c203
José Luis González Beltrán <joseluis.gonzalezbeltran@externals.adidas-group.com>
2019-02-14 17:39:49 +0100

    (fix) small fixes and remove not needed dependencies

--- docker-run.sh
+++ docker-run.sh
@@ -2,9 +2,8 @@
 
 mvn -f ./pom.xml clean package dockerfile:build
 docker rm -f coding-challenge-city-data
-docker run -d \
+docker run -d -p 8090:8080 \
     --name coding-challenge-city-data \
     --net codingchallenge \
-    -p 8090:8080 \
     -v ~/projects/coding-challenge/coding-challenge-city-data/target:/opt/lib \
     jgb/coding-challenge-city-data:latest
