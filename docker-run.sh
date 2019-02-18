dc329859b7918cf9557b9b9f54cca5471c9574f2
José Luis González Beltrán <joseluis.gonzalezbeltran@externals.adidas-group.com>
2019-02-18 10:15:08 +0100

    (feature) generate docker image with Makefile with jar file copied inside image

--- docker-run.sh
+++ docker-run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-mvn -f ./pom.xml clean package dockerfile:build
+export PROJECT_VERSION=0.0.1-SNAPSHOT
+make -C docker tag
 docker rm -f coding-challenge-city-data
 docker run -d -p 8090:8080 \
     --name coding-challenge-city-data \
     --net codingchallenge \
-    -v ~/projects/coding-challenge/coding-challenge-city-data/target:/opt/lib \
-    jgb/coding-challenge-city-data:latest
+    jgb/coding-challenge-city-data:${PROJECT_VERSION}
